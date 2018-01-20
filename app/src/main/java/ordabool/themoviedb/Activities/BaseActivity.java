d748f3e7a006bcdc33f0fa88a7c52a7537f71258
Or <or.dabool@gmail.com>
2018-01-20 23:39:35 +0200

    navigation view listener added

--- app/src/main/java/ordabool/themoviedb/Activities/BaseActivity.java
+++ app/src/main/java/ordabool/themoviedb/Activities/BaseActivity.java
@@ -1,11 +1,17 @@
 package ordabool.themoviedb.Activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import ordabool.themoviedb.R;
 
@@ -13,6 +19,7 @@ public class BaseActivity extends AppCompatActivity {
 
     FrameLayout frameLayout;
     DrawerLayout drawerLayout;
+    NavigationView navigationView;
     ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
@@ -20,7 +27,23 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
-        frameLayout = (FrameLayout)findViewById(R.id.content_frame);
+        frameLayout = findViewById(R.id.content_frame);
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent i;
+                switch (item.getItemId()) {
+                    case R.id.featuredMediaItem:
+                        Toast.makeText(BaseActivity.this, "Featured", Toast.LENGTH_SHORT).show();
+                        i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                        break;
+
+                }
+                return false;
+            }
+        });
 
         drawerLayout = findViewById(R.id.drawerLayout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
@@ -33,11 +56,9 @@ public class BaseActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         if (actionBarDrawerToggle.onOptionsItemSelected(item)){
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
